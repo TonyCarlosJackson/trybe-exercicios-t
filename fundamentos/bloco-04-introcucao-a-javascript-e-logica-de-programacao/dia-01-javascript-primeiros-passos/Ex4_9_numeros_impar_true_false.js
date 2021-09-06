6be43a00f055179b8061c78c52700840f062cf7c
antonio_carlos <acoliveira95@gmail.com>
2021-09-06 15:40:01 -0300

    Ex4_9

---
+++ fundamentos/bloco-04-introcucao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/Ex4_9_numeros_impar_true_false.js
@@ -0,0 +1,14 @@
+const x = 0;
+const y = 7;
+const z = 35;
+
+if ((x % 2 !== 0  || y % 2 !== 0 ||  z % 2 !== 0)) {
+    console.log("true");
+} else {
+    console.log("false");
+}    
+
+/*
+Exercício 9
+Escreva um programa que defina três números em constantes no seu começo e retorne true se pelo menos uma das três for ímpar. Caso contrário, ele deve retornar false .
+*/
