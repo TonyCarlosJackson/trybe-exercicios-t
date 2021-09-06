65a679ad84af6117216ff0279cb658a2f100e08d
antonio_carlos <acoliveira95@gmail.com>
2021-09-06 15:29:00 -0300

    Ex4_3

---
+++ fundamentos/bloco-04-introcucao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/Ex4_3_maior_de_tres.js
@@ -0,0 +1,17 @@
+const x = 23;
+const y = 24;
+const z = 35;
+
+if (x > y && x > z) {
+    console.log( + x + ' É o maior número');
+
+} else if (y > x && y > z) {
+    console.log( + y + ' É maior número');
+} else {
+    console.log( + z + ' É maior número');
+};        
+
+/*
+Exercício 3
+Faça um programa que retorne o maior de três números. Defina, no começo do programa, três constantes com os valores que serão comparados.
+*/
