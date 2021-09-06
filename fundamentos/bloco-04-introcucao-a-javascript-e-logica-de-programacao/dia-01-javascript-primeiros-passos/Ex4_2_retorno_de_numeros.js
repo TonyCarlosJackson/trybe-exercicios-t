8a293b8b5ac6a79304d608c4f68d6a9a34310a29
antonio_carlos <acoliveira95@gmail.com>
2021-09-06 15:27:49 -0300

    Ex4_2

---
+++ fundamentos/bloco-04-introcucao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/Ex4_2_retorno_de_numeros.js
@@ -0,0 +1,13 @@
+const x = 23;
+const y = 6;
+
+if (x > y) {
+   console.log("o 'x' é maior que o 'y'");  
+} else {
+   console.log("o 'y' é maior que o 'x'");
+};
+
+/*
+Exercício 2
+Faça um programa que retorne o maior de dois números. Defina, no começo do programa, duas constantes com os valores que serão comparados.
+*/
