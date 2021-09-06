8efa3a1c3b85a024c8a97e1b84286b0412c79190
antonio_carlos <acoliveira95@gmail.com>
2021-09-06 15:38:51 -0300

    Ex4_8

---
+++ fundamentos/bloco-04-introcucao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/Ex4_8_numeros_par_true_false.js
@@ -0,0 +1,15 @@
+const x = 2;
+const y = 5;
+const z = 23;
+
+if ((x % 2 === 0  || y % 2 === 0 ||  z % 2 === 0)) {
+    console.log("true");
+} else {
+    console.log("false");
+}
+
+/*
+Exercício 8
+Escreva um programa que defina três números em constantes e retorne true se pelo menos uma das três for par. Caso contrário, ele retorna false .
+Bonus: use somente um if .
+*/
