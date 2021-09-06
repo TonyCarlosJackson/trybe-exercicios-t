830a1363a3e469eab371dcf8d5e1be3092476d8e
antonio_carlos <acoliveira95@gmail.com>
2021-09-06 15:34:02 -0300

    Ex4_4

---
+++ fundamentos/bloco-04-introcucao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/Ex4_4_number_positive_negative.js
@@ -0,0 +1,15 @@
+const number = - 23;
+
+if (number > 0){
+  console.log("Número é positivo");
+} else if (number < 0){
+  console.log("Número é negativo"); 
+} else {
+  console.log("Número é zero");
+}; 
+
+/*
+Exercício 4
+Faça um programa que, dado um valor definido numa constante, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero", caso contrário.
+
+*/
