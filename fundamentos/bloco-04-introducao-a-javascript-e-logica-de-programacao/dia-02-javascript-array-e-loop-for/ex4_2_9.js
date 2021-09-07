c8ab28024c1590cfbd5fe1f04e230d8e02a3e99a
antonio_carlos <acoliveira95@gmail.com>
2021-09-07 11:29:23 -0300

    ex4_2_9

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/ex4_2_9.js
@@ -0,0 +1,6 @@
+for (let index = 1; index < 26; index +=1 ){
+    console.log(index / 2);
+}
+
+
+/* ex 9 - Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2 . */ 
