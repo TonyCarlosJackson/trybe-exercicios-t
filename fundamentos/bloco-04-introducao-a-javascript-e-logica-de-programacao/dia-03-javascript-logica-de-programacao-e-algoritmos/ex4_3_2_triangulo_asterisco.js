e11b1589e79c6bca360a3b1b61a7be31969dbea3
antonio_carlos <acoliveira95@gmail.com>
2021-09-07 20:47:31 -0300

    ex4_3_2

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/ex4_3_2_triangulo_asterisco.js
@@ -0,0 +1,15 @@
+let tamanho = 5;
+let linha = "";
+let simbolo = "*";
+
+
+
+for (let i = 0; i <= tamanho; i += 1) {
+    console.log(linha);
+    linha += simbolo;
+};
+
+
+
+
+/* exercicio 2 - Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base. Por exemplo: */
