96f1624a108859316780596f1e6e654b75e9203d
antonio_carlos <acoliveira95@gmail.com>
2021-09-07 20:42:37 -0300

    ex4_3_1

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/ex4_3_1_quadrado_asterisco.js
@@ -0,0 +1,16 @@
+let n = 7;
+let linha = "";
+let simbolo = "*";
+
+
+for (let i = 0; i < n; i += 1){
+    linha += simbolo; 
+}
+
+for (let i = 0; i < n; i += 1) {
+    console.log(linha);
+}
+
+
+
+/* exercicio 1 - Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n . Por exemplo: */
