e851547f877d67002a3c7c6f929d359595a7c402
antonio_carlos <acoliveira95@gmail.com>
2021-09-07 22:21:59 -0300

    exparte01_4_4_4

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/exparte01_4_4_4_key_respostas.js
@@ -0,0 +1,21 @@
+/*let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    recorrente: 'Sim',
+};
+
+for(let key in info){
+ console.log(info[key]);
+ 
+}
+
+/* exercicio 4 - Faça um novo for/in , mas agora mostre todos os valores das chaves do objeto. */
+
+let player = {
+ name: 'Marta',
+ lastName: 'Silva',
+ age: 34,
+ medals: { golden: 2, silver: 3 },
+
+};
