4dc8905da779e23a69b1494ef9b7574189150772
antonio_carlos <acoliveira95@gmail.com>
2021-09-07 22:20:40 -0300

    exparte01_4_4_3

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/exparte01_4_4_3_key.js
@@ -0,0 +1,15 @@
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    recorrente: 'Sim',
+};
+
+for(let key in info){
+ console.log(key);
+ 
+}
+
+/* exercicio 3 - Faça um for/in que mostre todas as chaves do objeto. 
+*/
+
