03bf28b389c7a00f4df7954062e33fb5ac09a5df
antonio_carlos <acoliveira95@gmail.com>
2021-09-07 21:56:37 -0300

    ex05_4_4_5

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/ex05_4_4_5_chave_medals.js
@@ -0,0 +1,15 @@
+let player = {
+    name: 'Marta',
+    lastName: 'Silva',
+    age: 34,
+    medals: { golden: 2, silver: 3 },
+    bestInTheWorld: [2006, 2007, 2008, 2009, 2010, 2018],
+
+  };
+
+  console.log('A jogadora possui ' + player.medals.golden + ' medalhas de ouro e ' + player.medals.silver + ' medalhas de prata.');
+
+
+/* exercicio 5 - Acesse a chave bestInTheWorld e faça um console.log no seguinte formato: "A jogadora Marta Silva foi eleita a melhor do mundo por 6 vezes".
+
+*/
