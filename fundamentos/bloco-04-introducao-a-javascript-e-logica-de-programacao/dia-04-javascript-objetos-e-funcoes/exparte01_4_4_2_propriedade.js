7839963b1760f81c8b7e6cd059cbbe9d9f99df4d
antonio_carlos <acoliveira95@gmail.com>
2021-09-07 22:02:20 -0300

    exparte01_4_4_2

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/exparte01_4_4_2_propriedade.js
@@ -0,0 +1,15 @@
+let info = {
+  personagem: "Margarida",
+  origem: "Pato Donald",
+  nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+ 
+};
+
+info['recorrente'] = 'Sim';
+info.recorrente = 'Sim';
+
+console.log(info);
+
+
+/* exercicio2 - Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console. Valor esperado no console: */ 
+
